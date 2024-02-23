7a5b686d1d746263ee6b8adacb02467a1fac7a41
Uroš Jureš <uros.jures@myworld.com>
2024-02-23 11:37:05 +0100

    welcome

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="/img/gzo-znak.png" alt="Gasilska Zveza Občine Kuzma" />
+  </div>
+</template>
+<style>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+}
+
+img {
+  height: 200px;
+  margin-top:50%;
+}
+</style>
