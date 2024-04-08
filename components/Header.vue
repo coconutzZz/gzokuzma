8954da86e2f886b5770b54361f31fdcfd0153f07
Uroš Jureš <uros.jures@myworld.com>
2024-04-08 09:21:26 +0200

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -3,5 +3,25 @@
     <NuxtLink to="/">
       <img src="/assets/img/gzo-znak.png" class="h-40 landscape:h-40 mr-3 sm:h-40" />
     </NuxtLink>
+    <nav v-if="headerMenu">
+        <ul class="flex space-x-8 text-lg font-bold">
+          <li v-for="blok in headerMenu" :key="blok._uid">
+            <NuxtLink :to="`/${(blok.link.url.length > 0 ? blok.link.url : blok.link.cached_url)}`" class="hover:text-[#50b0ae]">
+              {{ blok.link.story.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
   </header>
 </template>
+<script setup>
+const storyblokApi = useStoryblokApi()
+const { data } = await storyblokApi.get('cdn/stories/config', {
+  version: 'draft',
+  resolve_links: 'url',
+})
+ 
+const headerMenu = ref(null)
+headerMenu.value = data.story.content.header_menu
+</script>
+ 
