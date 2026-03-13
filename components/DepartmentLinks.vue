32114c2f12be62256bab235aa264b847e1e3dec0
btds\uj <u.jures@torschitz-schanes.at>
2026-03-13 13:42:14 +0100

    Revert "replace on error to prevent console logs"
    This reverts commit 00a3f29bc9ea7f3ce6422e5dbe162dcc3c9baf68.

--- components/DepartmentLinks.vue
+++ components/DepartmentLinks.vue
@@ -8,7 +8,7 @@
   <NuxtImg :src="`/img/${d.slug}.png`" 
   :class="d.is_main_unit ? 'h-10 md:h-16' : 'h-8 md:h-10'" 
     class="transition-transform duration-300 hover:scale-125" 
-      @error="e => (e.target.src='/img/pgddefault.png')"
+    placeholder="/img/pgddefault.png"
     />  
   <span :class="d.is_main_unit ? 'font-bold' : 'font-semibold'" class="text-primary font-semibold text-xs m-2 group-[.is-active]:font-bold">
     {{ d.name }}
