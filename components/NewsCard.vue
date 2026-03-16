6e043ce59fb1922893e606ffa71497b297ca2554
btds\uj <u.jures@torschitz-schanes.at>
2026-03-16 11:46:41 +0100

    revert to img - not working prod

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -6,7 +6,7 @@
         class="sm:w-full relative overflow-hidden rounded-l-lg sm:rounded-l-none sm:rounded-t-lg group">
           <div v-if="isLoading" class="w-full h-40 object-cover sm:rounded-t-lg animate-pulse bg-gray-300"></div>
           <NuxtLink v-else-if="hasImage" :to="`/novice/${article.slug}`">
-              <NuxtImg :src="article.content.image.filename + '/m/'" :alt="article.content.title" 
+              <img :src="article.content.image.filename + '/m/'" :alt="article.content.title" 
               class="w-full h-40 object-cover transform transition-transform duration-300 ease-out group-hover:scale-110" />
           </NuxtLink>
       </div>
